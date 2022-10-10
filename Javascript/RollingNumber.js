05aca97e7bfab56ff58a98611c31fd31d0a46288
Nonwhiskerscat <nonwhiskerscat@gmail.com>
2022-10-11 08:51:41 +0900

    ver10.2

---
+++ Javascript/RollingNumber.js
@@ -0,0 +1,20 @@
+window.onload=function(){
+    var memberCountConTxt= 144683;
+
+    $({ val : 0 }).animate({ val : memberCountConTxt }, {
+    duration: 3000,
+    step: function() {
+    var num = numberWithCommas(Math.floor(this.val));
+        $(".dial").text(num);
+    },
+    
+    complete: function() {
+        var num = numberWithCommas(Math.floor(this.val));
+        $(".dial").text(num);
+    }
+    });
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+}
