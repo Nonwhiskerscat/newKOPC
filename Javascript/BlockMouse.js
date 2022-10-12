3d66945bdd650c1316cd9d77cfe9f5d2e8793bde
Nonwhiskerscat <nonwhiskerscat@gmail.com>
2022-10-12 18:03:27 +0900

    ver16.0

--- Javascript/BlockMouse.js
+++ Javascript/BlockMouse.js
@@ -4,7 +4,7 @@ window.onload=function(){
         Swal.fire({
             title: "<h5 style='color:#e7a335'>" + '텍스트 도둑질 금지!' + "</h5>",
             html: "<p style='color:#fff; font-size:18px'>"+'텍스트 퍼가려다 나한테 제대로 걸렸냥? <br>한 번만 더 걸리면 가만 안 둔다냥! ฅ(=ↀДↀ=)ฅ'+"</p>",
-            imageUrl: './img/기타/alert1.png',
+            imageUrl: './img/기타/alert1(다크).png',
             imageWidth: '200px',
             imageHeight: '200px',
             imageAlt: 'Custom image',
@@ -20,7 +20,7 @@ window.onload=function(){
         Swal.fire({
             title: "<h5 style='color:#e7a335'>"+'오른쪽 마우스 클릭 금지!'+"</h5>",
             html: "<p style='color:#fff; font-size:18px'>"+'이미지 훔쳐 가지 말고 <br>우리 길고양이들 후원 좀 해주라냥! ฅ(=ↀᴥↀ=)ฅ'+"</p>",
-            imageUrl: './img/기타/alert1.png',
+            imageUrl: './img/기타/alert1(다크).png',
             imageWidth: '200px',
             imageHeight: '200px',
             imageAlt: 'Custom image',
